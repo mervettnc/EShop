0ca95fcbd3966cbbe4bdb2a7f3a317948461b700
Merve Tütüncü <mrvttnc96@gmail.com>
2020-06-12 20:23:59 +0300

    shopcontext

---
+++ src/Infrastructure/Data/Config/ProductConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data.Config
+{
+   public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id)
+               .ValueGeneratedOnAdd() //otomatik artan
+                .IsRequired();//aslında int olduğu için yazmaya gerek yok
+
+            builder.Property(x => x.ProductName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(x => x.UnitPrice)
+               .IsRequired(true)
+               .HasColumnType("decimal(18,2)");
+
+            builder.Property(x => x.PhotoPath)
+               .IsRequired(false);
+
+            builder.HasOne(x => x.Category)
+                .WithMany()
+                .HasForeignKey(x => x.CategoryId);
+
+            builder.HasOne(x => x.Brand)
+                .WithMany()
+                .HasForeignKey(x => x.BrandId);
+                
+
+           
+        }
+    }
+}
