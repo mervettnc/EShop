7f9a45b021223a8d1f68cf26465aca93f4d37a5c
Merve Tütüncü <mrvttnc96@gmail.com>
2020-06-15 22:09:29 +0300

    seed list

---
+++ src/Web/Services/HomeIndexViewModelService.cs
@@ -0,0 +1,64 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Interfaces;
+using Web.ViewModels;
+
+namespace Web.Services
+{
+    public class HomeIndexViewModelService : IHomeIndexViewModelService
+    {
+        private readonly IAsyncRepository<Category> _categoryRepository;
+        private readonly IAsyncRepository<Brand> _brandRepository;
+        private readonly IAsyncRepository<Product> _productRepository;
+
+        public HomeIndexViewModelService(IAsyncRepository<Category> categoryRepository,IAsyncRepository<Brand> brandRepository, IAsyncRepository<Product> productRepository)
+        {
+            _categoryRepository = categoryRepository;
+            _brandRepository = brandRepository;
+            _productRepository = productRepository;
+        }
+
+        public async Task<HomeIndexViewModel> GetHomeIndexViewModel(int? categoryId,int? brandId)
+        {
+            var vm = new HomeIndexViewModel
+            {
+                Categories=await GetSCategories(),
+                Brands=await GetSBrans(),
+                Products=await _productRepository.ListAllAsync(),
+                CategoryId=categoryId,
+                BrandId=brandId
+
+            };
+            return vm;
+        }
+
+        public async Task<List<SelectListItem>> GetSBrans()
+        {
+            var brands = await _brandRepository.ListAllAsync();
+            var items = brands
+                .Select(x => new SelectListItem() { Value = x.Id.ToString(), Text = x.BrandName })
+                .OrderBy(x => x.Text)
+                .ToList();
+            var allItem = new SelectListItem() { Value = null, Text = "All",Selected=true };
+            items.Insert(0,allItem);
+            return items;
+        }
+
+        public async Task<List<SelectListItem>> GetSCategories()
+        {
+            var categories = await _categoryRepository.ListAllAsync();
+            var items = categories
+                .Select(x => new SelectListItem() { Value = x.Id.ToString(), Text = x.CategoryName })
+                .OrderBy(x => x.Text)
+                .ToList();
+            var allItem = new SelectListItem() { Value = null, Text = "All" };
+            items.Insert(0, allItem);
+            return items;
+        }
+    }
+}
